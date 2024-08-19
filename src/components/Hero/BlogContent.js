4759752cbb64923099de90f795b9138e2c146f73
Peter <adelekepeterpierre@gmail.com>
2024-08-19 03:35:45 +0100

    added a js file for the slider

---
+++ src/components/Hero/BlogContent.js
@@ -0,0 +1,50 @@
+import image1 from "../../assets/images/13.jpg";
+import image2 from "../../assets/images/1.png";
+import image3 from "../../assets/images/2.png";
+import image4 from "../../assets/images/5.png";
+import image5 from "../../assets/images/15.webp";
+
+const blogSnip = [
+  {
+    id: 1,
+    img: image1,
+    headline: "Benefits of Cross-Training for Runners",
+    link: "https://coolrunningsfitnessblog.com/benefits-of-cross-training-for-runners/",
+    trans: "slide-up",
+    time: "100",
+  },
+  {
+    id: 2,
+    img: image2,
+    headline: "Endurance Running for Beginners",
+    link: "https://coolrunningsfitnessblog.com/endurance-running-for-beginners/",
+    trans: "slide-up",
+    time: "300",
+  },
+  {
+    id: 3,
+    img: image3,
+    headline: "Treating Common Running Injuries: A Comprehensive Guide",
+    link: "https://coolrunningsfitnessblog.com/treating-common-running-injuries-a-comprehensive-guide/",
+    trans: "slide-up",
+    time: "500",
+  },
+  {
+    id: 4,
+    img: image4,
+    headline: "Interval Training: Boost Your Running Performance",
+    link: "https://coolrunningsfitnessblog.com/interval-training-boost-your-running-performance/",
+    trans: "slide-up",
+    time: "300",
+  },
+  {
+    id: 5,
+    img: image5,
+    headline: "Mastering Your Running Pace on Race Day",
+    link: "https://coolrunningsfitnessblog.com/mastering-your-running-pace-on-race-day/",
+    trans: "slide-up",
+    time: "100",
+  },
+];
+
+export default blogSnip;
