3777209e664302907f42e1330299962ef2c29c40
Peter <adelekepeterpierre@gmail.com>
2024-08-19 03:35:08 +0100

    modified styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = ({ toggleForm }) => {
   return (
     <div id="contact" className="">
-      <div className="container dark:bg-dark bg-gray-100 py-14">
+      <div className="container dark:bg-black  py-14">
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
           <div className="col-span-2">
             <h1 className="text-1xl sm:text-3xl font-bold text-black/80 dark:text-white">
